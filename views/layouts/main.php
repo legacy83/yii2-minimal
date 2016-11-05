10e144a20da6247538d135c36609e4d40ab410b8
Thiago Senna <trsenna@gmail.com>
2016-11-05 13:07:48 -0200

    layout refactoring done

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -3,43 +3,46 @@
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-use yii\helpers\Html;
-use app\assets\AppAsset;
-
-AppAsset::register( $this );
+\app\assets\AppAsset::register( $this );
 ?>
+
 <?php $this->beginPage(); ?>
-    <!DOCTYPE html>
-    <html lang="<?= Yii::$app->language; ?>">
-    <head>
-        <meta charset="<?= Yii::$app->charset; ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?= Html::csrfMetaTags(); ?>
-        <title><?= Html::encode( $this->title ); ?></title>
-        <?php $this->head(); ?>
-    </head>
-    <body>
-    <?php $this->beginBody(); ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language; ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset; ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= \yii\helpers\Html::csrfMetaTags(); ?>
+    <title><?= \yii\helpers\Html::encode( $this->title ); ?></title>
+    <?php $this->head(); ?>
+</head>
+<body>
+<?php $this->beginBody(); ?>
 
-    <div class="container">
-        <div class="header clearfix">
-            <nav>
-                <ul class="nav nav-pills pull-right">
-                    <li role="presentation"><a href="<?= \yii\helpers\Url::home(); ?>">Home</a></li>
-                    <li role="presentation"><a href="<?= \yii\helpers\Url::toRoute( 'site/about' ); ?>">About</a></li>
-                </ul>
-            </nav>
-            <h3 class="text-muted">Yii2 Minimal</h3>
-        </div>
-
-        <?= $content; ?>
-
-        <footer class="footer">
-            <p>&copy; <?= date( 'Y' ); ?> Company, Inc.</p>
-        </footer>
+<div class="container">
+    <div class="header clearfix">
+        <nav>
+            <?= \yii\widgets\Menu::widget( [
+                'options' => [ 'class' => 'nav nav-pills pull-right' ],
+                'items' => [
+                    [ 'label' => 'Home', 'url' => [ 'site/index' ] ],
+                    [ 'label' => 'About', 'url' => [ 'site/about' ] ],
+                ],
+            ] ); ?>
+        </nav>
+        <h3 class="text-muted">
+            <?= \yii\helpers\Html::a( 'Yii2 Minimal', \yii\helpers\Url::to( [ 'site/index' ] ) ); ?>
+        </h3>
     </div>
 
-    <?php $this->endBody(); ?>
-    </body>
-    </html>
+    <?= $content; ?>
+
+    <footer class="footer">
+        <p>&copy; <?= date( 'Y' ); ?> Untitled, Inc.</p>
+    </footer>
+</div>
+
+<?php $this->endBody(); ?>
+</body>
+</html>
 <?php $this->endPage(); ?>
