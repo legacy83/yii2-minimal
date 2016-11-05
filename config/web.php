cf4f6185b1594c85f35a63593bebb400d5b30508
Thiago Senna <trsenna@gmail.com>
2016-11-04 22:39:37 -0200

    removed a lot of obsolete files

--- config/web.php
+++ config/web.php
@@ -1,43 +1,42 @@
 <?php
 
-$params = require(__DIR__ . '/params.php');
+$params = require( __DIR__ . '/params.php' );
 
 $config = [
-    'id' => 'basic',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
+    'id' => 'yii2-minimal',
+    'basePath' => dirname( __DIR__ ),
+    'bootstrap' => [ 'log' ],
     'components' => [
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'cookieValidationKey@secret',
         ],
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-        'user' => [
-            'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
-        ],
+//        'cache' => [
+//            'class' => 'yii\caching\FileCache',
+//        ],
+//        'user' => [
+//            'identityClass' => 'app\models\User',
+//            'enableAutoLogin' => TRUE,
+//        ],
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-        ],
+//        'mailer' => [
+//            'class' => 'yii\swiftmailer\Mailer',
+//            // send all mails to a file by default. You have to set
+//            // 'useFileTransport' to false and configure a transport
+//            // for the mailer to send real emails.
+//            'useFileTransport' => TRUE,
+//        ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
                 [
                     'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
+                    'levels' => [ 'error', 'warning' ],
                 ],
             ],
         ],
-        'db' => require(__DIR__ . '/db.php'),
+        'db' => require( __DIR__ . '/db.php' ),
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
@@ -50,15 +49,15 @@ $config = [
     'params' => $params,
 ];
 
-if (YII_ENV_DEV) {
+if ( YII_ENV_DEV ) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
+    $config[ 'bootstrap' ][] = 'debug';
+    $config[ 'modules' ][ 'debug' ] = [
         'class' => 'yii\debug\Module',
     ];
 
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
+    $config[ 'bootstrap' ][] = 'gii';
+    $config[ 'modules' ][ 'gii' ] = [
         'class' => 'yii\gii\Module',
     ];
 }
